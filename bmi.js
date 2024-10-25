a368b71e2264ec1758aee8d846e387403dfa7f76
Hridoykhan4 <hridoykhan148385@gmail.com>
2024-10-25 06:17:05 +0600

    BMI added

---
+++ bmi.js
@@ -0,0 +1,31 @@
+const resultDiv = document.getElementById('result');
+
+function calculateBMI(){
+    const age = document.getElementById('age').value;
+    const weight = parseFloat(document.getElementById('weight').value);
+    const height = parseFloat(document.getElementById('height').value) / 100; 
+
+    if (age && weight && height) {
+        const bmi = (weight / (height * height)).toFixed(2); 
+        
+        resultDiv.innerHTML = `
+        <p>Your Age is: ${age}</p>
+        <p>Your BMI is: ${bmi}</p>
+        ${getBMICategory(bmi)}
+        `;
+    } else {
+        resultDiv.innerHTML = "<p>Please fill in all fields.</p>";
+    }
+}
+
+function getBMICategory(bmi) {
+    if (bmi < 18.5) {
+        return "<p>You are underweight.</p>";
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        return "<p>Your weight is normal.</p>";
+    } else if (bmi >= 25 && bmi < 29.9) {
+        return "<p>You are overweight.</p>";
+    } else {
+        return "<p>You are obese.</p>";
+    }
+}

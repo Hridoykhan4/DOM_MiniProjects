0df9169cc781a7b881697f9465176a0a29bd1272
Hridoykhan4 <hridoykhan148385@gmail.com>
2024-11-06 01:27:26 +0600

    Expense Calculator ended

---
+++ expense_calculator.js
@@ -0,0 +1,26 @@
+document.getElementById('expense-insert-btn').addEventListener('click', function(){
+    const expenseInput = document.getElementById('expense-field').value;
+    const amountFields = getInputNumberById('amount-field');
+
+    const row = `
+    <div class="flex justify-between">
+    <p>${expenseInput}</p>
+    <p>${amountFields}</p>
+    </div>
+    `
+    const rowContainer = document.getElementById('row-container')
+  
+    rowContainer.innerHTML += row;
+
+    let expenseAmount = parseFloat(document.getElementById('expense-amount').innerText);
+    document.getElementById('expense-amount').innerText = expenseAmount + amountFields
+
+    const expensea = document.getElementById('expense-field');
+    console.log(expensea.getAttribute('id'))
+
+})
+
+function getInputNumberById(id){
+    const element = document.getElementById(id).value;
+    return Number(element);
+}

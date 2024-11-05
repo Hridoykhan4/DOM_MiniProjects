ef79a3708ac8d08b120bd345439bb59f53738b05
Hridoykhan4 <hridoykhan148385@gmail.com>
2024-11-05 07:08:52 +0600

    counter added

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+// Important Counter App 
+/* const html = document.getElementsByTagName('html')[0];
+html.setAttribute('data-theme', 'light')
+
+    const count = document.getElementById('count');
+
+    const counter = document.getElementById('counter')
+
+    const increaseBtn = document.getElementsByClassName('increase-button')[0];
+    increaseBtn.addEventListener('click', function(e){
+        let currentCount = Number(count.innerText);
+        currentCount++;
+        count.innerText = currentCount;
+        e.stopPropagation()
+        
+    })
+    
+    const decreaseBtn = document.getElementsByClassName('decrease-button')[0];
+    decreaseBtn.addEventListener('click', function(e){
+        let currentCount = Number(count.innerText);
+        currentCount--;
+        count.innerText = currentCount;
+        e.stopPropagation()
+        
+    })
+    document.getElementById('counter').addEventListener('click', function(){
+
+        if(decreaseBtn.getAttribute('disabled') === null){
+            increaseBtn.setAttribute('disabled', true);
+            decreaseBtn.setAttribute('disabled', true);
+            counter.classList.add("bg-red-500");
+            counter.classList.add("text-white");
+        }
+        else{
+            increaseBtn.removeAttribute('disabled');
+            decreaseBtn.removeAttribute('disabled');
+            counter.classList.remove('bg-red-500');
+            counter.classList.remove('text-white')
+        }
+    })
+ */
+
+
 /* let sum = 0;
 
 const plusBtn = document.getElementById('plus-btn')
